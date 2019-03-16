50d5995782ba6f293ba41c88f0af770e9501da6d
Antonio <antoniolucas123@hotmail.com>
2019-03-16 20:00:37 -0300

    Alterações Dia 16/03/2019 19:59 - Inserção da Biblioteca JQuery e Inserção da Biblioteca SLICK para a criação de Slide Show

--- _js/main.js
+++ _js/main.js
@@ -1,4 +1,21 @@
-//Variables
+$(document).ready(function () {
+
+    //Abrir e Fechar o Menu
+    $(".header__botao-menu").click(function(){
+        $(".header").toggleClass("header--menu-closed");
+    });
+
+    //Configurar o SLICK tela principal
+    $(".works-to-future-item-slick").slick({
+        arrows: true,
+        dots: true,
+        autoplay: true
+    });
+}); 
+
+// INTERACAO DO MENU COM JAVA SCRIPT PURO
+
+/*//Variables
 
 let menuResponsive = document.querySelector(".header");
 let buttonMenuResponsive = document.querySelector(".header__botao-menu");
@@ -11,4 +28,4 @@ buttonMenuResponsive.addEventListener("click", clickButtonMenuResponsive);
 
 function clickButtonMenuResponsive(event){
     menuResponsive.classList.toggle("header--menu-closed");
-}
+}*/
